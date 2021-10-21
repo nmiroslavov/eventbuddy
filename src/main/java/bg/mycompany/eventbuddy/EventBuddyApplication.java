0feaed75819c561daecfd31c2a401c6f361897dd
Nikola Nikolov <n.miroslavov@protonmail.com>
2021-10-21 23:36:16 +0300

    init project

---
+++ src/main/java/bg/mycompany/eventbuddy/EventBuddyApplication.java
@@ -0,0 +1,13 @@
+package bg.mycompany.eventbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventBuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventBuddyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bg/mycompany/eventbuddy/EventBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.mycompany.eventbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventBuddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
